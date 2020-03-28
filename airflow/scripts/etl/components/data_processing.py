9d1d84d5e0bf1c57cca9786fccb79c338984092f
jamesang <jamesang717@gmail.com>
2020-03-28 14:34:57 -0400

    remove records with empty values

--- airflow/scripts/etl/components/data_processing.py
+++ airflow/scripts/etl/components/data_processing.py
@@ -9,11 +9,16 @@ clean tweet text and write as new json object.
 """
 
 def transform_tweet(tweet_json, json_form):
-    json_form["tweet_id"] = tweet_json["id"]
+    if (not tweet_json["id_str"] or not tweet_json["created_at"] or
+        not tweet_json["user"]["screen_name"] or not tweet_json["text"] or 
+        not tweet_json["user"]["location"] or not tweet_json["favorite_count"]
+        or not tweet_json["retweet_count"]):
+        return {}
+    json_form["tweet_id"] = tweet_json["id_str"]
     json_form["created_at"] = tweet_json["created_at"]
     json_form["screen_name"] = tweet_json["user"]["screen_name"]
     json_form["text"] = tweet_cleaning_for_sentiment_analysis(tweet_json["text"].lower())
-    json_form["location"] = tweet_json["user"]["location"] if tweet_json["user"]["location"] else "N/A"
+    json_form["location"] = tweet_json["user"]["location"]
     json_form["favorite_count"] = tweet_json["favorite_count"]
     json_form["retweet_count"] = tweet_json["retweet_count"]
     return json_form
@@ -24,12 +29,13 @@ def preprocess(input, output, keep=1):
         line = input_file.readline()
         while line:
             json_form = {
-                "tweet_id":0,"created_at": "","screen_name": "","text": "",
+                "tweet_id":"","created_at": "","screen_name": "","text": "",
                 "location": "","favorite_count": "","retweet_count": ""
             }
             tweet_json = json.loads(line)
             row = transform_tweet(tweet_json, json_form)
-            output_file.write(json.dumps(row) + '\n')
+            if row:
+                output_file.write(json.dumps(row) + '\n')
             line = input_file.readline()
         output_file.close()
         input_file.close()
